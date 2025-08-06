098f116145c03e0ce44293b46b3e5b41c614155d
Passos <passosmatheus.work@gmail.com>
2025-08-06 17:54:12 -0300

    feat: implementando verificação de saúde.

--- worker/payment_processor.py
+++ worker/payment_processor.py
@@ -6,6 +6,9 @@ from HTTP.aiohtpp_session import get_aiohttp_session
 _DEFAULT_URL: str = 'http://payment-processor-default:8080/payments'
 _FALLBACK_URL: str = 'http://payment-processor-fallback:8080/payments'
 
+_DEFAULT_HEALTH_URL: str = 'http://payment-processor-default:8080/payments/service-health'
+_FALLBACK_HEALTH_URL: str = 'http://payment-processor-fallback:8080/payments/service-health'
+
 async def process_payment_in_default_processor(payload, _timeout: Optional[float] = 1.5):
     try:
         session = await get_aiohttp_session()
@@ -39,5 +42,37 @@ async def process_payment_in_fallback_processor(payload, _timeout: Optional[floa
                 response.raise_for_status()
 
         return payload
+    except Exception as e:
+        raise e
+
+async def get_payment_processor_default_health_status(_timeout: Optional[float] = 1.5):
+    try:
+        session = await get_aiohttp_session()
+        async with session.get(
+                _DEFAULT_HEALTH_URL,
+                timeout=aiohttp.ClientTimeout(total=_timeout)
+        ) as response:
+            if response.status != 200:
+                response.raise_for_status()
+
+            status = response.json()
+
+            return status
+    except Exception as e:
+        raise e
+
+async def get_payment_processor_fallback_health_status(_timeout: Optional[float] = 1.5):
+    try:
+        session = await get_aiohttp_session()
+        async with session.get(
+                _FALLBACK_HEALTH_URL,
+                timeout=aiohttp.ClientTimeout(total=_timeout)
+        ) as response:
+            if response.status != 200:
+                response.raise_for_status()
+
+            status = response.json()
+
+            return status
     except Exception as e:
         raise e
