b25810c7a0e9450fc43311105aa245ae0124cb5e
Passos <passosmatheus.work@gmail.com>
2025-08-06 17:17:55 -0300

    feat: criando processors simples e usando nats como queue pra aproveitar os queue groups e evitar que instancias peguem os mesmos payments da fila.

---
+++ worker/payment_processor.py
@@ -0,0 +1,43 @@
+import aiohttp
+
+from typing import Optional
+from HTTP.aiohtpp_session import get_aiohttp_session
+
+_DEFAULT_URL: str = 'http://payment-processor-default:8080/payments'
+_FALLBACK_URL: str = 'http://payment-processor-fallback:8080/payments'
+
+async def process_payment_in_default_processor(payload, _timeout: Optional[float] = 1.5):
+    try:
+        session = await get_aiohttp_session()
+        async with session.post(
+                _DEFAULT_URL,
+                json=payload,
+                timeout=aiohttp.ClientTimeout(total=_timeout)
+        ) as response:
+            if response.status == 422:
+                return "ignored"
+
+            if response.status != 200:
+                response.raise_for_status()
+
+        return payload
+    except Exception as e:
+        raise e
+
+async def process_payment_in_fallback_processor(payload, _timeout: Optional[float] = 1.5):
+    try:
+        session = await get_aiohttp_session()
+        async with session.post(
+                _FALLBACK_URL,
+                json=payload,
+                timeout=aiohttp.ClientTimeout(total=_timeout)
+        ) as response:
+            if response.status == 422:
+                return "ignored"
+
+            if response.status != 200:
+                response.raise_for_status()
+
+        return payload
+    except Exception as e:
+        raise e
