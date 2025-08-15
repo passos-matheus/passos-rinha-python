a2ecfd772f4b877438963134d4da37189e01d308
Passos <passosmatheus.work@gmail.com>
2025-08-15 17:45:28 -0300

    fix: corrigindo tratamento pra bytes.

--- worker/api/intern_queue.py
+++ worker/api/intern_queue.py
@@ -33,8 +33,9 @@ async def process_queue(worker_id, _queue, tcp_client):
 
 async def process_batch(batch, worker_id, _queue, tcp_client):
     semaphore = semaphores[worker_id]
-    batch_json = json.dumps(batch)
 
+    decoded_batch = [item.decode("utf-8") for item in batch]
+    batch_json = json.dumps(decoded_batch)
     async with semaphore:
         return await tcp_client.send_batch_payments(batch_json)
 
