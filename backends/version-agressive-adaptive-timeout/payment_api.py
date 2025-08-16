e20247f3d983c11f7a36e65cc8e5eaf3ee2ca7ef
MatheusPassoss <matheuspassos.work@gmail.com>
2025-08-16 03:00:50 -0300

    feat: adicionando código fonte da segunda submissão.

---
+++ backends/version-agressive-adaptive-timeout/payment_api.py
@@ -0,0 +1,138 @@
+import os
+import uvicorn
+import asyncio
+import logging
+
+from starlette.responses import Response
+
+from setup import lifespan
+from payment_queue import queue
+from utils.redis_client import redis_client
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse, PlainTextResponse
+
+from utils.utils import (
+    logger,
+    get_redis_range,
+    calculate_summary,
+    LogFilter,
+    iso_to_timestamp,
+    REDIS_TIMEOUT
+)
+
+
+app = FastAPI(lifespan=lifespan)
+
+RESPONSE_MESSAGES = {
+    "accepted": {"status": "accepted"},
+    "purged": {"status": "purged"},
+    "empty_body": {"error": "empty body"},
+    "queue_full": {"error": "queue full"}
+}
+
+@app.post('/payments', status_code=201)
+async def create_payment(request: Request) -> Response:
+    try:
+        body = await request.body()
+
+        if not body:
+            raise HTTPException(
+                status_code=400,
+                detail=RESPONSE_MESSAGES["empty_body"]
+            )
+
+        queue.put_nowait(body)
+
+        return Response(status_code=201)
+    except asyncio.QueueFull:
+        return Response(status_code=503)
+    except Exception as e:
+        return Response(status_code=500, content=e)
+
+@app.get('/payments-summary')
+async def get_payments_summary(request: Request) -> JSONResponse:
+    try:
+        from_ = request.query_params.get('from')
+        to = request.query_params.get('to')
+
+        min_score = '-inf'
+        max_score = '+inf'
+
+        if from_:
+            timestamp, success = iso_to_timestamp(from_)
+            if success and timestamp is not None:
+                min_score = timestamp
+
+        if to:
+            timestamp, success = iso_to_timestamp(to)
+            if success and timestamp is not None:
+                max_score = timestamp
+
+        default_task = asyncio.create_task(
+            get_redis_range('default', min_score, max_score)
+        )
+        fallback_task = asyncio.create_task(
+            get_redis_range('fallback', min_score, max_score)
+        )
+
+        try:
+            default_items, fallback_items = await asyncio.wait_for(
+                asyncio.gather(default_task, fallback_task),
+                timeout=REDIS_TIMEOUT
+            )
+        except asyncio.TimeoutError:
+            raise
+    except:
+        default_items, fallback_items = [], []
+
+    default_summary = await calculate_summary(default_items, 'default')
+    fallback_summary = await calculate_summary(fallback_items, 'fallback')
+
+    return JSONResponse(
+        status_code=200,
+        content={
+            'default': default_summary,
+            'fallback': fallback_summary,
+        }
+    )
+
+@app.post('/purge-payments')
+async def purge_payments() -> JSONResponse:
+    try:
+        await redis_client.delete('default', 'fallback')
+
+        return JSONResponse(
+            status_code=200,
+            content=RESPONSE_MESSAGES["purged"]
+        )
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail={"error": f"Failed to purge data: {str(e)}"}
+        )
+
+@app.get('/health')
+async def health_check() -> PlainTextResponse:
+    return PlainTextResponse('OK\n')
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.CRITICAL)
+    logging.disable(logging.INFO)
+
+    port = int(os.getenv('PORT', '8080'))
+    host = '0.0.0.0'
+
+    uvicorn.run(
+        'payment_api:app',
+        host=host,
+        port=port,
+        reload=False,
+        log_level="critical",
+        access_log=False,
+        use_colors=False,
+        loop="uvloop",
+        http="httptools",
+        backlog=2048,
+    )
+
---
+++ backends/version-agressive-adaptive-timeout/payment_worker.py
@@ -0,0 +1,189 @@
+import os
+import json
+import time
+import asyncio
+
+from datetime import datetime
+from typing import Any
+
+from utils.redis_client import redis_client
+from payment_queue import queue
+from health_checker import check_health_routine
+from HTTP.aiohtpp_session import cleanup_session
+from payment_processor import process_payment_in_default_processor, process_payment_in_fallback_processor
+
+# 12980, duas instâncias
+# NUM_WORKERS = 4
+# CONCURRENT_REQUESTS = 6
+# MAX_BATCH_SIZE = 250
+
+# {
+# config:
+# NUM_WORKERS = 4
+# CONCURRENT_REQUESTS = 8
+# MAX_BATCH_SIZE = 250
+# resultado servido de testes
+# R$ 310.228,07
+# Bruto: R$ 332.827,50
+# R$ 0,00
+# 41.73ms
+# }
+
+# {
+# config:
+# NUM_WORKERS = 4
+# CONCURRENT_REQUESTS = 16
+# MAX_BATCH_SIZE = 250
+# resultado servido de testes
+# R$
+# Bruto:
+# R$
+#
+# }
+
+NUM_WORKERS = 4
+CONCURRENT_REQUESTS = 8
+MAX_BATCH_SIZE = 250
+
+semaphores = {}
+_datetime_cache = datetime.now().isoformat()
+_last_update = time.time()
+
+def cached_datetime():
+    global _datetime_cache, _last_update
+    now = time.time()
+
+    if now - _last_update > 0.05:
+        _datetime_cache = datetime.now().isoformat()
+        _last_update = now
+
+    return _datetime_cache
+
+async def process_queue(worker_id):
+    semaphores[worker_id] = asyncio.Semaphore(CONCURRENT_REQUESTS)
+
+    while True:
+        batch = []
+
+        item = await queue.get()
+        batch.append(item)
+
+        while len(batch) < MAX_BATCH_SIZE:
+            try:
+                item = await asyncio.wait_for(queue.get(), timeout=0.008)
+                batch.append(item)
+            except asyncio.TimeoutError:
+                break
+
+        await process_batch(batch, worker_id)
+        # await asyncio.sleep(0)
+        for _ in batch:
+            queue.task_done()
+
+async def process_batch(batch, worker_id):
+    semaphore = semaphores[worker_id]
+
+    best_processor = 1
+    best_timeout = 5.1
+
+    health_status = await redis_client.get('health_processors')
+    if health_status:
+        status = json.loads(health_status)
+        # best_processor = status["best"]
+        # best_timeout = status["timeout"] + 0.05
+
+        if status['default']['failing']:
+            await asyncio.sleep(5)
+
+    async def run_with_semaphore(payment_json):
+        async with semaphore:
+            return await process_payment(payment_json, best_processor, best_timeout)
+
+    tasks = [run_with_semaphore(payment) for payment in batch]
+    await asyncio.gather(*tasks, return_exceptions=True)
+
+
+async def process_payment(payment_json: Any, best_processor: int, best_timeout: float):
+    max_retries = 3
+    payment_processed = False
+    processor_used = None
+
+    try:
+        payment = json.loads(payment_json)
+        payload = {
+            "correlationId": str(payment["correlationId"]),
+            "amount": payment["amount"],
+            "requestedAt": cached_datetime()
+        }
+
+        for attempt in range(max_retries):
+            try:
+                if best_processor == 1:
+                    resp = await process_payment_in_default_processor(payload, best_timeout)
+                    if resp == "ignored":
+                        break
+                    processor_used = "default"
+                    payment_processed = True
+                else:
+                    resp = await process_payment_in_fallback_processor(payload, best_timeout)
+                    if resp == "ignored":
+                        break
+                    processor_used = "fallback"
+                    payment_processed = True
+
+                break
+
+            except Exception as e:
+                error_str = str(e).lower()
+                is_retryable = any(keyword in error_str for keyword in ['timeout', 'connection', 'refused'])
+
+                if is_retryable and attempt < max_retries - 1:
+                    await asyncio.sleep(0.1 * (attempt + 1))
+                    continue
+                elif is_retryable and attempt == max_retries - 1:
+                    await queue.put(payment_json)
+                else:
+                    raise
+
+        if payment_processed and processor_used:
+            await save_payment(payload, processor_used)
+
+    except (KeyError, ValueError, TypeError):
+        return "parse_error"
+    except Exception as e:
+        await queue.put(payment_json)
+
+async def save_payment(payload, key):
+    if not key:
+        raise Exception
+
+    correlation_id = payload.get('correlationId')
+    if not correlation_id:
+        raise Exception("correlationId is required")
+
+    processed_key = f"{key}:processed_ids"
+
+    is_processed = await redis_client.sismember(processed_key, correlation_id)
+    if is_processed:
+        return
+
+    pipeline = redis_client.pipeline()
+
+    score = int(datetime.fromisoformat(payload['requestedAt']).timestamp() * 1000)
+    member = json.dumps(payload)
+
+    pipeline.zadd(key, {member: score})
+    pipeline.sadd(processed_key, correlation_id)
+
+    await pipeline.execute()
+
+async def run_workers():
+    try:
+        consumers = [process_queue(i) for i in range(NUM_WORKERS)]
+        await asyncio.gather(
+            check_health_routine(),
+            *consumers
+        )
+    finally:
+        await cleanup_session()
+
