0689b28e67557a9113cd5142ecb32741a211860c
MatheusPassoss <matheuspassos.work@gmail.com>
2025-08-16 16:52:17 -0300

    fix: removendo cat.

--- backends/version-agressive-adaptive-timeout/utils/utils.py
+++ backends/version-agressive-adaptive-timeout/utils/utils.py
@@ -1,18 +1,3 @@
-import os
-
-from redis.asyncio.client import Redis
-from redis.asyncio.connection import ConnectionPool
-
-redis_pool = ConnectionPool(
-    host=os.getenv("REDIS_HOST", "redis-db"),
-    port=6379,
-    max_connections=100,
-    decode_responses=True,
-    socket_timeout=5
-)
-
-redis_client = Redis(connection_pool=redis_pool)
-root@20d382050d56:/app/utils# cat utils.py 
 import re
 import math
 import orjson
